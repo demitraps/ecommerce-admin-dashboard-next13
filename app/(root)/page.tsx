17e0f6e25cc2121a060b2d49810d5a8246accb85
demitra <hellodemitra@gmail.com>
2023-07-29 17:02:18 +0300

    Project Setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <Button size="default" variant="outline">Click me</Button>
+    </div>
+  );
+}
