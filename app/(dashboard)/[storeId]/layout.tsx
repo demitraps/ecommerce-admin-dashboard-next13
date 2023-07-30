5f88461a9c51f2544a0565cffadeafd8d4fce63d
demitra <hellodemitra@gmail.com>
2023-07-30 17:38:39 +0300

    Store Navigation bar

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import prismadb from "@/lib/prismadb";
+import Navbar from "@/components/navbar";
 
 export default async function DashboardLayout({
   children,
@@ -30,7 +31,7 @@ export default async function DashboardLayout({
   return (
     <>
       <div>
-        This will be a navbar
+        <Navbar />
         {children}
       </div>
     </>
---
+++ components/main-nav.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useParams, usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+
+export function MainNav({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) {
+  const pathname = usePathname();
+  const params = useParams();
+  const routes = [
+    {
+      href: `/${params.storeId}/settings`,
+      label: 'Settings',
+      active: pathname === `/${params.storeId}/settings`
+    },
+  ];
+  return (
+   <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
+    {routes.map((route) => (
+      <Link
+      key={route.href}
+      href={route.href}
+      className={cn("text-sm font-medium transition-colors hover:text-primary",
+       route.active ? "text-black dark:text-white" : "text-muted-foreground")}
+      >
+      {route.label}
+      </Link>
+    ))}
+
+
+   </nav>
+  )
+}
